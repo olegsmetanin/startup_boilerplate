1192652e1a0d2a9785a03e075629d295f418692e
Oleg Smetanin <oleg@smetan.in>
2017-12-03 23:26:03 +0300

    fix race on dep init

--- api.dev.sh
+++ api.dev.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-go get -u github.com/golang/dep/cmd/dep
-dep init
-dep ensure -update
-go get github.com/tockins/realize
+go get -u github.com/tockins/realize
 realize start --install --run --name="api"
 
--- protowatch.dev.sh
+++ protowatch.dev.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+go get -u github.com/golang/dep/cmd/dep
+dep init
+dep ensure -update
 reflex -g serviceproto/*.proto -s -- sh -c 'protoc -I serviceproto/ --go_out=plugins=grpc:serviceproto serviceproto/*.proto & protoc -I serviceproto/ --plugin=protoc-gen-ts=/usr/bin/protoc-gen-ts --ts_out=service=true:serviceproto serviceproto/*.proto && cp -r serviceproto/ front/'
 
